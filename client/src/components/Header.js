4b63a4d25569a84ccb56a015201326db6de44b2a
Alisson Rios <alisrios@gmail.com>
2024-10-30 20:55:49 -0300

    mudando texto do botão adicionar add

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "green" : "purple"}
-          text={showAdd ? "Fechar" : "Adicionar"}
+          text={showAdd ? "Fechar" : "Add"}
           onClick={onAdd}
         />
       )}
