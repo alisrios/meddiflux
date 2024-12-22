83988a34c76c5844f9828695031989a1389a95d3
doancasotti <doancasotti@outlook.com>
2024-12-22 20:01:47 -0300

    alterando melhorias

--- ARQUIVOS_ERRADOS/gerar-compose-hom.sh
+++
@@ -1,3 +0,0 @@
-cp -f docker-compose-hom.yml docker-compose.yml
-
-echo "docker-compose.yml gerado com sucesso para o ambiente Hom!"
--- ARQUIVOS_ERRADOS/gerar-compose-prod.sh
+++
@@ -1,3 +0,0 @@
-cp -f docker-compose-prod.yml docker-compose.yml
-
-echo "docker-compose.yml gerado com sucesso para o ambiente Prod!"
